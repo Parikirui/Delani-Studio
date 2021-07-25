852846396d8f1fc7392bf7907845d276dc34d3de
Parikirui <isaac.kirui@student.moringaschool.com>
2021-07-25 17:56:01 +0300

    modify the html and the css code and add new js file

--- js/script.js
+++ js/script.js
@@ -17,3 +17,54 @@ $(document).ready(function () {
 
     });
 });
+
+$(document).ready(function () {
+    $("div#development").click(function(event) {
+        event.preventDefault();
+
+        $("div#development").toggle();
+        $("p#developmentText").toggle();
+        $("div#developmentLabel").show();
+
+    });
+
+    $("p#developmentText").click(function(event) {
+        event.preventDefault();
+
+        $("div#development").toggle();
+        $("p#developmentText").toggle();
+        $("div#developmentLabel").hide();
+
+
+    });
+});
+
+$(document).ready(function () {
+    $("div#productManagement").click(function(event) {
+        event.preventDefault();
+
+        $("div#productManagement").toggle();
+        $("p#productManagementText").toggle();
+        $("div#productManagementLabel").show();
+        
+    });
+
+    $("p#productManagementText").click(function(event) {
+        event.preventDefault();
+
+        $("div#productManagement").toggle();
+        $("p#productManagementText").toggle();
+        $("div#productManagementLabel").hide();
+        
+    });
+});
+
+$(document).ready(function() {
+   $(".portfolioImages").mouseenter(function(){
+    let portfolioImages = $(this).closest('.portfolioImages');
+    portfolioImages.next("h3").show();
+   })
+   .mouseleave(function(){
+    $("h3").hide();
+   });
+});
